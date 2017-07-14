6a82997299489135482bdc4c115f01ebd88dd94a
Craig Hollabaugh <holla2040@gmail.com>
2017-07-14 01:39:51 -0600

    added get and set offsets added get and set offsets to example

--- AD2/__init__.py
+++ AD2/__init__.py
@@ -24,6 +24,11 @@ class AD2(object):
 
         self.open() 
 
+        self._vposSetOffset = 0.0
+        self._vposGetOffset = 0.0
+        self._vnegSetOffset = 0.0
+        self._vnegSetOffset = 0.0
+
         self.dwf.FDwfAnalogIOChannelNodeSet(self.hdwf,c_int(0),c_int(0),c_double(True)) # enable positive supply
         self.dwf.FDwfAnalogIOChannelNodeSet(self.hdwf,c_int(1),c_int(0),c_double(True)) # enable negative supply
         self.dwf.FDwfAnalogIOEnableSet(self.hdwf,c_int(True))
@@ -37,29 +42,71 @@ class AD2(object):
     def close(self):
         self.dwf.FDwfDeviceClose(self.hdwf)
 
+
+
+
     @property
     def vpos(self):
         v = c_double()
         self.dwf.FDwfAnalogIOChannelNodeGet(self.hdwf,c_int(0),c_int(1),byref(v))
-        return v.value
+        return v.value + self._vposGetOffset
 
     @vpos.setter
     def vpos(self,value):
-        self.dwf.FDwfAnalogIOChannelNodeSet(self.hdwf,c_int(0),c_int(1),c_double(value))
+        self.dwf.FDwfAnalogIOChannelNodeSet(self.hdwf,c_int(0),c_int(1),c_double(value+self._vposSetOffset))
+
+    @property
+    def vposSetOffset(self):
+        return self._vposSetOffset
+
+    @vposSetOffset.setter
+    def vposSetOffset(self,value):
+        self._vposSetOffset = value
+
+    @property
+    def vposGetOffset(self):
+        return self._vposGetOffset
+
+    @vposGetOffset.setter
+    def vposGetOffset(self,value):
+        self._vposGetOffset = value
+
+
+
+
+
+
+
+
 
     @property
     def vneg(self):
         v = c_double()
         self.dwf.FDwfAnalogIOChannelNodeGet(self.hdwf,c_int(1),c_int(1),byref(v))
-        return v.value
+        return v.value + self._vnegGetOffset
 
     @vneg.setter
     def vneg(self,value):
-        self.dwf.FDwfAnalogIOChannelNodeSet(self.hdwf,c_int(1),c_int(1),c_double(value))
+        self.dwf.FDwfAnalogIOChannelNodeSet(self.hdwf,c_int(1),c_int(1),c_double(value+self._vnegSetOffset))
+
+    @property
+    def vnegSetOffset(self):
+        return self._vnegSetOffset
+
+    @vnegSetOffset.setter
+    def vnegSetOffset(self,value):
+        self._vnegSetOffset = value
+
+    @property
+    def vnegGetOffset(self):
+        return self._vnegGetOffset
+
+    @vnegGetOffset.setter
+    def vnegGetOffset(self,value):
+        self._vnegGetOffset = value
+
         
 
 if __name__ == "__main__":
     ad = AD2()
     print "version",ad.dwfversion
-
-
--- examples/powersupplies.py
+++ examples/powersupplies.py
@@ -5,9 +5,20 @@ sys.path.append('..')
 import AD2
 
 d = AD2.AD2()
-for i in range(0,51):
-    d.vpos = i/10.0
-    d.vneg = -i/10.0
-    print "%5.2f %5.2f %5.2f"%(i/10.0,d.vpos,d.vneg)
-    time.sleep(0.25)
+
+d.vposSetOffset = 0.0007
+d.vposGetOffset = -0.0007
+d.vpos = 1.23
+
+d.vnegSetOffset = 0.0049
+d.vnegGetOffset = 0.1195
+d.vneg = -1.23
+
+time.sleep(0.5)
+
+print "%8.4f %8.4f %8.4f"%(d.vpos,d.vposSetOffset,d.vposGetOffset)
+print "%8.4f %8.4f %8.4f"%(d.vneg,d.vnegSetOffset,d.vnegGetOffset)
+
+raw_input("\nany key to continue... ")
+
 d.close()
